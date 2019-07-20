063a42b8aad452fd3173dd2c1c531b938e96dc0a
M A R M <seanbrucet@gmail.com>
2019-07-19 22:09:48 -0500

    yolo

--- pages/index.vue
+++ pages/index.vue
@@ -302,7 +302,7 @@
             <div class="row">
               <!--Grid column-->
               <div class="col-md-9 mb-md-0 mb-5">
-                <form id="contact-form" name="contact-form" action="mail.php" method="POST" netlify>
+                <form id="contact-form" name="contact-form" action="mail.php" method="POST" >
                   <!--Grid row-->
                   <div class="row">
                     <!--Grid column-->
